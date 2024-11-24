ce52b3cf0fc2300002383c266a912282e3eb3ee6
Swe-bhi <swetha.sundar@chasacademy.se>
2024-11-25 00:14:18 +0100

    Committing the edited file

--- main.tf
+++ main.tf
@@ -9,7 +9,16 @@ terraform {
 
 provider "azurerm" {
   features {}
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
+
+variable "client_id" {}
+variable "client_secret" {}
+variable "subscription_id" {}
+variable "tenant_id" {}
 
 resource "azurerm_resource_group" "rg" {
   name     = "myResourceGroup"
