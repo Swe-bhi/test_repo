940abbaa3082e71e28c2a7c472b1a0d1230c0ff4
Swe-bhi <swetha.sundar@chasacademy.se>
2024-11-25 08:33:45 +0100

    Pushing image code changes

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ resource "azurerm_virtual_machine" "vm" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "22.04-LTS"
+    sku       = "20.04-LTS"
     version   = "latest"
   }
 }
